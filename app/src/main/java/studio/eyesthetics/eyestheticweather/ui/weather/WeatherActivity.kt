c38b8c73160244da67c0bd1bd12438b4ecae6f6b
Spinking <spinking@mail.ru>
2019-08-07 11:50:52 +0400

    first commit

---
+++ app/src/main/java/studio/eyesthetics/eyestheticweather/ui/weather/WeatherActivity.kt
@@ -0,0 +1,16 @@
+package studio.eyesthetics.eyestheticweather.ui.weather
+
+/**
+ * Created by BashkatovSM on 07.08.2019
+ */
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import studio.eyesthetics.eyestheticweather.R
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
